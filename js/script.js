a358737ea2f0f6ba12e8b54e636772a30fab6260
Rafal Szymanko <szymanko@gmail.com>
2020-03-05 17:59:38 +0100

    Add result script

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ let playerMove = ``;
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 let playerInput = prompt(`Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce`);
 
-
 if (randomNumber == 1) {
     computerMove = `kamień`;
 } else if (randomNumber == 2) {
@@ -16,8 +15,33 @@ if (playerInput == 1) {
     playerMove = `kamień`;
 } else if (playerInput == 2) {
     playerMove = `papier`;
-} else {
+} else if (playerInput == 3) {
     playerMove = `nożyce`;
+} else {
+    playerMove = `nieznany ruch`;
 }
 
-msg = printMessage(`Zagrałem ${computerMove}! Jeśli Twój ruch to papier, to wygrywasz!`);
+printMessage(`Twój ruch: ${playerMove}`);
+printMessage(`Ruch komputera: ${computerMove}`);
+
+
+if (playerMove == `papier` && computerMove == `kamień`) {
+    printMessage(`Wygrałeś!!!`);
+} else if (playerMove == `papier` && computerMove == `nożyce`) {
+    printMessage(`Przegrałeś :((`);
+} else if (playerMove == `kamień` && computerMove == `papier`) {
+    printMessage(`Przegrałeś :((`);
+} else if (playerMove == `kamień` && computerMove == `nożyce`) {
+    printMessage(`Wygrałeś!!!`);
+} else if (playerMove == `nożyce` && computerMove == `kamień`) {
+    printMessage(`Przegrałeś :((`);
+} else if (playerMove == `nożyce` && computerMove == `papier`) {
+    printMessage(`Wygrałeś!!!`);
+} else if (playerMove == `nieznany ruch`) {
+    alert(`Wybierz wartość od 1 do 3!!!`)
+}
+else {
+    printMessage(`Remis`);
+}
+
+
