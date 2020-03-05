8e685aacee716fb10da167f1198e78052a010be6
Rafal Szymanko <szymanko@gmail.com>
2020-03-05 17:13:51 +0100

    Add script for computer move

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-let computerMove = `kamień`;
-let playerMove = `papier`
+let computerMove = ``;
+let playerMove = ``;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let playerInput = prompt(`Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce`);
+
+
+console.log (randomNumber)
+
+if (randomNumber == 1) {
+    computerMove = `kamień`;
+} else if (randomNumber == 2) {
+    computerMove = `papier`;
+} else {
+    computerMove = `nożyce`;
+}
+
+if (playerInput == 1) {
+    playerMove = `kamień`;
+} else if (playerInput == 2) {
+    playerMove = `papier`;
+} else {
+    playerMove = `nożyce`;
+}
 
 msg = printMessage(`Zagrałem ${computerMove}! Jeśli Twój ruch to papier, to wygrywasz!`);
-
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
