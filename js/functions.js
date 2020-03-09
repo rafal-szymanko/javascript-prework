4ed73e384f654d499382df78c750f0d9c2bd0fd2
Rafal Szymanko <szymanko@gmail.com>
2020-03-09 20:48:40 +0100

    Add result counter

--- js/functions.js
+++ js/functions.js
@@ -1,50 +1,73 @@
-function printMessage(msg) {
+function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);	
-};
+	if (msg == `Wygrałeś!!!`) {
+		document.getElementById('messages').appendChild(div).style.color = 'green'; 
+	} else if (msg == `Przegrałeś :((`)  {
+		document.getElementById('messages').appendChild(div).style.color = 'red'; 
+	} else if (msg == `Remis`) {
+		document.getElementById('messages').appendChild(div).style.color = 'grey';
+	} else {
+		document.getElementById('messages').appendChild(div);
+	}
+}
 
-function clearMessages() {
+function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
-};
+}
+
+function showResult() {
+	printMessage(`Rozegrane partie ${gamePlayed}`);
+	printMessage(`Wygrane partie: ${gameWon}`);
+	printMessage(`Przegrane partie: ${gameLost}`);
+	printMessage(`Remis: ${gameDrew}`);
+}
+
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+	  return 'kamień';
+	} else if (argMoveId == 2) {
+		return 'papier';
+	} else if (argMoveId == 3) {
+		return 'nożyce';
+	}
+  }
+
+function displayResult(argComputerMove, argPlayerMove){
+	gamePlayed++;
+	printMessage(`Zagrałem ${argComputerMove}, a Ty ${argPlayerMove}`);
+  
+	if (argPlayerMove == `papier` && argComputerMove == `kamień`) {
+		printMessage(`Wygrałeś!!!`);
+		gameWon++;
+	} else if (argPlayerMove == `papier` && argComputerMove == `nożyce`) {
+		printMessage(`Przegrałeś :((`);
+		gameLost++;
+	} else if (argPlayerMove == `kamień` && argComputerMove == `papier`) {
+		printMessage(`Przegrałeś :((`);
+		gameLost++;
+	} else if (argPlayerMove == `kamień` && argComputerMove == `nożyce`) {
+		printMessage(`Wygrałeś!!!`);
+		gameWon++;
+	} else if (argPlayerMove == `nożyce` && argComputerMove == `kamień`) {
+		printMessage(`Przegrałeś :((`);
+		gameLost++;
+	} else if (argPlayerMove == `nożyce` && argComputerMove == `papier`) {
+		printMessage(`Wygrałeś!!!`);
+		gameWon++;
+	} 
+	else  {
+		printMessage(`Remis`);
+		gameDrew++;
+	}
+}
 
 function playGame(playerInput) {
 	let randomNumber = Math.floor(Math.random() * 3 + 1);
 	let computerMove = getMoveName(randomNumber);
 	let playerMove = getMoveName(playerInput);
-
 	clearMessages();
-	
-	function getMoveName(argMoveId){
-		if(argMoveId == 1){
-		  return 'kamień';
-		} else if (argMoveId == 2) {
-			return 'papier';
-		} else if (argMoveId == 3) {
-			return 'nożyce';
-		}
-	  }
-	
-	function displayResult(argComputerMove, argPlayerMove){
-		printMessage(`Zagrałem ${argComputerMove}, a Ty ${argPlayerMove}`);
-	  
-		if (playerMove == `papier` && argComputerMove == `kamień`) {
-			printMessage(`Wygrałeś!!!`);
-		} else if (argPlayerMove == `papier` && argComputerMove == `nożyce`) {
-			printMessage(`Przegrałeś :((`);
-		} else if (argPlayerMove == `kamień` && argComputerMove == `papier`) {
-			printMessage(`Przegrałeś :((`);
-		} else if (argPlayerMove == `kamień` && argComputerMove == `nożyce`) {
-			printMessage(`Wygrałeś!!!`);
-		} else if (argPlayerMove == `nożyce` && argComputerMove == `kamień`) {
-			printMessage(`Przegrałeś :((`);
-		} else if (argPlayerMove == `nożyce` && argComputerMove == `papier`) {
-			printMessage(`Wygrałeś!!!`);
-		} 
-		else  {
-			printMessage(`Remis`);
-		}
-	}
-
+	getMoveName();
 	displayResult(computerMove, playerMove);
+	showResult()
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let gamePlayed = 0;
+let gameWon = 0;
+let gameLost = 0;
+let gameDrew = 0;
+
+
+
 document.getElementById('play-rock').addEventListener('click', function() {
 	playGame(1);
 });
@@ -7,3 +14,5 @@ document.getElementById('play-paper').addEventListener('click', function() {
 document.getElementById('play-scissors').addEventListener('click', function() {
 	playGame(3);
 });
+
+showResult();
